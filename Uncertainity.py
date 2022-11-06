f45154a1aa11d2c4818674cc2a7478d2c617787e
AliJnadi <90157234+AliJnadi@users.noreply.github.com>
2022-11-07 02:18:10 +0300

    Update Uncertainity.py
    Add the swing_up mode

--- Uncertainity.py
+++ Uncertainity.py
@@ -18,33 +18,60 @@ I = 0.01 # inertia of actuator
 b = 0.08 # friction in actuator 
 dt = 0.01 # step value
 
+mode = 'swing_up'
+
 # define the environment
-env = Pendulum(m=m, L=L, I=I, b=b, dt=dt, mode='balance')
+env = Pendulum(m=m, L=L, I=I, b=b, dt=dt, mode=mode)
 env.reset()
 
-# Loading model after trainig
-model_path = './models'
-PPO_path = os.path.join(model_path, 'Pendulum_balance_model')
-model = PPO.load(PPO_path, env=env)
-
-# Evaluating the model by changing the mass randomlly and plot the result
 eval_rslt = []
 eval_m = []
 success = 0
 max_n_episodes = 100
 m_org = env.m
-for _ in range(0, max_n_episodes):
-    env.m = m_org*(1 + 0.2*np.random.uniform(-1, +1))
-    eval_m.append(env.m)
-    eval_rslt.append(evaluate_policy(model, env, n_eval_episodes=1, render=False)[0])
-    if eval_rslt[-1] > 195: 
-        success += 1
-    else:
-        print(env.m)
 
-success = 100*success / max_n_episodes
-plt.plot(eval_rslt)
-plt.title(f'Evaluating of Pendulum in balance mode, success = {success}%')
-plt.xlabel('Episode')
-plt.ylabel('Return')
-plt.show()
+model_path = './models'
+# Loading model after trainig
+if mode == 'balance':
+    PPO_path = os.path.join(model_path, 'Pendulum_balance_model')
+
+    model = PPO.load(PPO_path, env=env)
+
+    # Evaluating the model by changing the mass randomlly and plot the result
+    for _ in range(0, max_n_episodes):
+        env.m = m_org*(1 + 0.2*np.random.uniform(-1, +1))
+        eval_m.append(env.m)
+        eval_rslt.append(evaluate_policy(model, env, n_eval_episodes=1, render=False)[0])
+        if eval_rslt[-1] > 195: 
+            success += 1
+        else:
+            print(env.m)
+
+    success = 100*success / max_n_episodes
+    plt.plot(eval_rslt)
+    plt.title(f'Evaluating of Pendulum in balance mode, success = {success}%')
+    plt.xlabel('Episode')
+    plt.ylabel('Return')
+    plt.show()
+
+elif mode == 'swing_up':
+    PPO_path = os.path.join(model_path, 'Pendulum_swing_up_model')
+
+    model = PPO.load(PPO_path, env=env)
+
+    # Evaluating the model by changing the mass randomlly and plot the result
+    for _ in range(0, max_n_episodes):
+        env.m = m_org*(1 + 0.2*np.random.uniform(-1, +1))
+        eval_m.append(env.m)
+        eval_rslt.append(evaluate_policy(model, env, n_eval_episodes=1, render=False)[0])
+        if eval_rslt[-1] > -1100: 
+            success += 1
+        else:
+            print(env.m)
+
+    success = 100*success / max_n_episodes
+    plt.plot(eval_rslt)
+    plt.title(f'Evaluating of Pendulum in swing_up mode, success = {success}%')
+    plt.xlabel('Episode')
+    plt.ylabel('Return')
+    plt.show()

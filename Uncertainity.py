068640ccb929ba1f2d1a4c7c1fbee1d37839e95a
AliJnadi <90157234+AliJnadi@users.noreply.github.com>
2022-11-06 16:26:47 +0300

    Add files via upload
    Load the model and test it for 100 episods, printing the return, in each episode we change the mass randomally (20%).

---
+++ Uncertainity.py
@@ -0,0 +1,50 @@
+# Import required libraries
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+import os
+
+import numpy as np
+
+from Pendulum import Pendulum
+
+import matplotlib.pyplot as plt
+
+# set the parameter of our pendulum
+m = 1 # mass of the pendulum bob
+g = 9.81 # gravity acceleration
+L = 0.2 # length of pendulum rod
+I = 0.01 # inertia of actuator
+b = 0.08 # friction in actuator 
+dt = 0.01 # step value
+
+# define the environment
+env = Pendulum(m=m, L=L, I=I, b=b, dt=dt, mode='balance')
+env.reset()
+
+# Loading model after trainig
+model_path = './models'
+PPO_path = os.path.join(model_path, 'Pendulum_balance_model')
+model = PPO.load(PPO_path, env=env)
+
+# Evaluating the model by changing the mass randomlly and plot the result
+eval_rslt = []
+eval_m = []
+success = 0
+max_n_episodes = 100
+m_org = env.m
+for _ in range(0, max_n_episodes):
+    env.m = m_org*(1 + 0.2*np.random.uniform(-1, +1))
+    eval_m.append(env.m)
+    eval_rslt.append(evaluate_policy(model, env, n_eval_episodes=1, render=False)[0])
+    if eval_rslt[-1] > 195: 
+        success += 1
+    else:
+        print(env.m)
+
+success = 100*success / max_n_episodes
+plt.plot(eval_rslt)
+plt.title(f'Evaluating of Pendulum in balance mode, success = {success}%')
+plt.xlabel('Episode')
+plt.ylabel('Return')
+plt.show()
